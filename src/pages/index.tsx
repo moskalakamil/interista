82e3254d5d3f81f2246249df70e46fb5b8897ecd
szymonrybczak <szymon.rybczak@gmail.com>
2023-02-20 11:31:19 +0100

    ci: setup tests

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import { Inter } from "@next/font/google";
-import Header from "@/components/landing/Header/Index";
+import Header from "./../components/landing/Header/Index";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
