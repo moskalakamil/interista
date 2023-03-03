311688665f205a2c76c4a4eccfa6b34b1ac59308
kacper-adamczyk <90323565+kacper-adamczyk@users.noreply.github.com>
2023-03-03 13:14:32 +0100

    Fix/articles/newtitles (#19)
    Co-authored-by: kacper-adamczyk <kacperadamczyk@MacBook-Air-kacper.local>

--- src/components/landing/Articles/Index.tsx
+++ src/components/landing/Articles/Index.tsx
@@ -4,37 +4,37 @@ import Article from "./Article";
 
 const dummyArticles = [
   {
-    name: "Łał wygrana",
+    name: "Niesamowity sukces drużyny!",
     time: "15 lipca 2020",
     tag: "Gorący piłkazyk",
     id: 1,
   },
   {
-    name: "Tomasz Hajto Pierdolnoł babe na pasach",
+    name: "Niestety porażka",
     time: "10 czerwca 2022",
     tag: "Gorący piłkazyk",
     id: 2,
   },
   {
-    name: "Sprzedam opla",
+    name: "Piękny debiut młodego zawodnika",
     time: "10 lipca 2020",
     tag: "Gorący piłkazyk",
     id: 3,
   },
   {
-    name: "Lubie grać w piłke nozną",
+    name: "Sędzia ewidętnie się pomylił",
     time: "10 lipca 2020",
     tag: "Gorący piłkazyk",
     id: 4,
   },
   {
-    name: "Trawa jest zielona",
+    name: "Kibice przygotowali piękną oprawę",
     time: "10 lipca 2020",
     tag: "Gorący piłkazyk",
     id: 5,
   },
   {
-    name: "gram w gre",
+    name: "Wygrana na swoim terenie.",
     time: "10 lipca 2020",
     tag: "Gorący piłkazyk",
     id: 6,
