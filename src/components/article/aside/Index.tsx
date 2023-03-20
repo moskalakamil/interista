935a195cc68193ae33533440670c19db03f09685
moskalakamil <moskalakamil07@gmail.com>
2023-03-20 10:32:01 +0100

    style: add aside container style

--- src/components/article/aside/Index.tsx
+++ src/components/article/aside/Index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 import Ads from "./Ads";
 import Author from "./Author";
 import LastArticles from "./LastArticles";
@@ -12,12 +13,17 @@ interface Props {
 const Aside = ({ author }: Props) => {
   console.log(author);
   return (
-    <>
+    <DivStyled>
       <Author author={author} />
       <LastArticles />
       <Ads />
-    </>
+    </DivStyled>
   );
 };
 
 export default Aside;
+
+const DivStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
