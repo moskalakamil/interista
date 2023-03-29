d2965191fb14b0940136c77206de5d090c2c881f
fnwk <filip.wnek200@gmail.com>
2023-03-29 13:00:58 +0200

    feat: add getPostId query

---
+++ src/pages/api/post/getPostsId.ts
@@ -0,0 +1,24 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from "./../../../../lib/prisma";
+import { Post } from "@/types/posts";
+import { Response } from "@/types/Response";
+
+export const getPosts = async () => {
+  const postsId = await prisma.post.findMany({
+    take: 50,
+    select: {
+      id: true,
+    },
+  });
+
+  return postsId.map(({ id }: { id: string }) => id);
+};
+
+export default async function handler({}: NextApiRequest, res: NextApiResponse<Post[] | Response>) {
+  try {
+    const posts = await getPosts();
+    res.status(200).json(posts);
+  } catch (e) {
+    return res.status(500).send({ message: e });
+  }
+}
